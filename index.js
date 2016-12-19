0f2fba996f7f48f835fe4d4c13632e064ca285f1
qingwei.li <cinwell.li@gmail.com>
2016-12-19 13:36:08 +0800

    Remove emitWarning

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var webpack = require('webpack')
+var isNextWebpack = false
+try {
+  isNextWebpack = require('cooking/util/check').isNextWebpack
+} catch(_) {}
+
 module.exports = function (cooking) {
   cooking.add('preLoader.js', {
     test: /\.jsx?$/,
@@ -12,8 +18,10 @@ module.exports = function (cooking) {
   })
 
   cooking.config.eslint = {
-    formatter: require('eslint-friendly-formatter'),
-    // eslint error 会打断 webpack build 过程
-    emitWarning: true
+    formatter: require('eslint-friendly-formatter')
+  }
+  if (!isNextWebpack) {
+    // eslint error 会打断 webpack@1 build 过程
+    cooking.config.eslint.emitWarning = true
   }
 }
